c61f6db0b734bc7e39a016b6399b363297c50101
Jerusa de Souza <jerusa24souza@gmail.com>
2025-03-19 08:26:14 -0300

    Erro do CashOut

--- public/js/home.js
+++ public/js/home.js
@@ -108,30 +108,30 @@ function getCashIn() {
 function getCashOut() {
     const transactions = data.transactions;
 
-    const cashIn = transactions.filter((item) => item.type === "2");
+    const cashOut = transactions.filter((item) => item.type === "2");
 
-    if (cashIn.length) {
-        let cashInhtml = ``;
+    if (cashOut.length) {
+        let cashOuthtml = ``;
         let limit = 0;
 
-        if (cashIn.length > 5) {
+        if (cashOut.length > 5) {
             limit = 5;
         } else {
-            limit = cashIn.length;
+            limit = cashOut.length;
         }
 
         for (let index = 0; index < limit; index++) {
-            cashInhtml += `
+            cashOuthtml += `
             <div class="row mb-4">
                 <div class="col-12">
-                    <h3 class="fs-2">R$ ${cashIn[index].value, tofixed(2)}</h3>
+                    <h3 class="fs-2">R$ ${cashOut[index].value.toFixed(2)}</h3>
                     <div class="container p-0">
                         <div class="row">
                             <div class="col-12 col-md-8">
-                                <p>${cashIn[index].description}</p>
+                                <p>${cashOut[index].description}</p>
                             </div>
                             <div class="col-12 col-md-3 d-flex justify-content-end">
-                                ${cashIn[index].date}
+                                ${cashOut[index].date}
                             </div>
                         </div>
                     </div>
@@ -140,7 +140,7 @@ function getCashOut() {
             `
         }
 
-        document.getElementById("cash-out-list").innerHTML = cashInhtml;
+        document.getElementById("cash-out-list").innerHTML = cashOuthtml;
     }
 }
 
